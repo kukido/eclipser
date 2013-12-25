736bfff1416a8f375140303736b7b2e2f7725fea
kukido <myatlyuk@gmail.com>
2013-12-25 14:33:48 -0800

    Updated test cases

--- test/com/kukido/eclipser/command/AddMaven2ConfigurationCommandTest.java
+++ test/com/kukido/eclipser/command/AddMaven2ConfigurationCommandTest.java
@@ -1,17 +1,57 @@
 package com.kukido.eclipser.command;
 
 import com.intellij.execution.RunManager;
+import com.intellij.execution.configurations.RunConfiguration;
 import com.intellij.execution.impl.RunManagerImpl;
+import com.intellij.ide.plugins.PluginManager;
+import com.intellij.openapi.project.Project;
 import com.intellij.testFramework.LightIdeaTestCase;
+import com.kukido.eclipser.configuration.Maven2Configuration;
+import org.jetbrains.idea.maven.execution.MavenRunConfiguration;
 
 public class AddMaven2ConfigurationCommandTest extends LightIdeaTestCase {
 
-    public static final String TEST_CASE_MODULE_NAME = "light_idea_test_case";
-
     private AddMaven2ConfigurationCommand command;
 
-    // todo: add test cases
+    /*
+    public void testExecuteWithFullConfiguration() throws Exception {
+        Maven2Configuration configuration = new Maven2Configuration(
+                "kukido-test",
+                true,
+                new String[]{"local", "remote"},
+                "clean compile"
+        );
 
+        configuration.getCommand();
+
+        command = new AddMaven2ConfigurationCommand(configuration);
+
+        Project project = getProject();
+        command.execute(project);
+
+        validateCreatedConfiguration(configuration);
+    }
+    */
+
+    private void validateCreatedConfiguration(Maven2Configuration configuration) {
+        MavenRunConfiguration mavenRunConfiguration = null;
+        RunManager manager = RunManager.getInstance(getProject());
+        // todo: java: getAllConfigurations() in com.intellij.execution.RunManager has been deprecated
+        RunConfiguration[] configurations = manager.getAllConfigurations();
+        for (RunConfiguration runConfiguration : configurations) {
+            if (runConfiguration.getName().equals(configuration.getConfigurationName())) {
+                if (runConfiguration instanceof MavenRunConfiguration) {
+                    mavenRunConfiguration = (MavenRunConfiguration)runConfiguration;
+                }
+            }
+        }
+
+        if (mavenRunConfiguration == null) {
+            fail("Maven run configuration was not created");
+        } else {
+            assertEquals(configuration.getConfigurationName(), mavenRunConfiguration.getName());
+        }
+    }
 
     @Override
     protected void tearDown() throws Exception {
