7894be13510640fdf291b5f4d955e6d3cfa1ac25
Andrey Myatlyuk <andrey@step.com>
2024-06-16 20:12:47 -0700

    Version update, Ant update

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,11 @@
 plugins {
     id("java")
-    id("org.jetbrains.intellij") version "1.16.0"
+    id("org.jetbrains.intellij") version "1.17.3"
     id("org.jetbrains.kotlin.jvm") version "1.9.10"
 }
 
 group = "org.jetbrains"
-version = "0.9.3"
+version = "0.9.4"
 
 repositories {
     mavenCentral()
@@ -17,11 +17,11 @@ java {
 
 // See https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
 intellij {
-    version.set("2023.2")
+    version.set("2024.1.3")
     plugins.set(listOf(
         "com.intellij.java",
         "org.jetbrains.idea.maven",
-        "AntSupport",
+        "AntSupport:241.17011.48",
     ))
 }
 
@@ -32,7 +32,7 @@ tasks {
 
     patchPluginXml {
         version.set("${project.version}")
-        sinceBuild.set("232")
+        sinceBuild.set("241")
     }
 
     compileKotlin {
